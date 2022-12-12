378e20b4c51bf124b5ed013c6857819781159011
Midnight44 <61278626+Midnight44@users.noreply.github.com>
2022-12-12 10:51:17 -0500

    0.0.4
    Step 4 complete, was missing {Observable, of}

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES} from './mock-heroes';
+import { MessageService } from './message.service';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  getHeroes(): Observable <Hero[]> {
+  
+  constructor(private messageService: MessageService) { }
+  
+    getHeroes(): Observable <Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
-  constructor() { }
 
 }
